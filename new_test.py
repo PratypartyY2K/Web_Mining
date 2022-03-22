f6f8b9ec49a83f8ed8328807b8893819a777d8ae
PratypartyY2K <kumarkinshuk14@gmail.com>
2022-03-22 22:12:49 +0530

    Added python code to find page rank

--- new_test.py
+++ new_test.py
@@ -1,35 +1,75 @@
 import networkx as nx
+G=nx.barabasi_albert_graph(60,41)
+pr = nx.pagerank(G,max_iter = 7)
+print(pr)
 
-def betweenness_centrality(G, k=None, normalized=True, weight=None,
-						endpoints=False, seed=None):
-	betweenness = dict.fromkeys(G, 0.0) # b[v]=0 for v in G
-	if k is None:
-		nodes = G
+def pagerank(G, personalization=None,
+			max_iter=max_iter, tol=1.0e-6, nstart=None, weight='weight',
+			dangling=None):
+	
+	if len(G) == 0:
+		return {}
+
+	if not G.is_directed():
+		D = G.to_directed()
 	else:
-		nx.random.seed(seed)
-		nodes = nx.random.sample(G.nodes(), k)
-	for s in nodes:
+		D = G
 
-		# single source shortest paths
-		if weight is None: # use BFS
-			S, P, sigma = nx._single_source_shortest_path_basic(G, s)
-		else: # use Dijkstra's algorithm
-			S, P, sigma = nx._single_source_dijkstra_path_basic(G, s, weight)
+	# Create a copy in (right) stochastic form
+	W = nx.stochastic_graph(D, weight=weight)
+	N = W.number_of_nodes()
 
-		# accumulation
-		if endpoints:
-			betweenness = nx._accumulate_endpoints(betweenness, S, P, sigma, s)
-		else:
-			betweenness = nx._accumulate_basic(betweenness, S, P, sigma, s)
+	# Choose fixed starting vector if not given
+	if nstart is None:
+		x = dict.fromkeys(W, 1.0 / N)
+	else:
+		# Normalized nstart vector
+		s = float(sum(nstart.values()))
+		x = dict((k, v / s) for k, v in nstart.items())
 
-	# rescaling
-	betweenness = nx._rescale(betweenness, len(G), normalized=normalized,
-						directed=G.is_directed(), k=k)
-	return betweenness
+	if personalization is None:
 
-G=nx.DiGraph()
-G.add_nodes_from([0,6])
-edges = [(0,1),(0,2),(0,3),(1,3),(1,4),(2,5),(3,5),(3,6),(4,3),(4,6),(6,5)]
-G.add_edges_from(edges)
-b=nx.betweenness_centrality(G)
-print(b)
+		# Assign uniform personalization vector if not given
+		p = dict.fromkeys(W, 1.0 / N)
+	else:
+		missing = set(G) - set(personalization)
+		if missing:
+			raise nx.NetworkXError('Personalization dictionary '
+								'must have a value for every node. '
+								'Missing nodes %s' % missing)
+		s = float(sum(personalization.values()))
+		p = dict((k, v / s) for k, v in personalization.items())
+
+	if dangling is None:
+
+		# Use personalization vector if dangling vector not specified
+		dangling_weights = p
+	else:
+		missing = set(G) - set(dangling)
+		if missing:
+			raise nx.NetworkXError('Dangling node dictionary '
+								'must have a value for every node. '
+								'Missing nodes %s' % missing)
+		s = float(sum(dangling.values()))
+		dangling_weights = dict((k, v/s) for k, v in dangling.items())
+	dangling_nodes = [n for n in W if W.out_degree(n, weight=weight) == 0.0]
+
+	# power iteration: make up to max_iter iterations
+	for _ in range(max_iter):
+		xlast = x
+		x = dict.fromkeys(xlast.keys(), 0)
+		danglesum = alpha * sum(xlast[n] for n in dangling_nodes)
+		for n in x:
+
+			# this matrix multiply looks odd because it is
+			# doing a left multiply x^T=xlast^T*W
+			for nbr in W[n]:
+				x[nbr] += alpha * xlast[n] * W[n][nbr][weight]
+			x[n] += danglesum * dangling_weights[n] + (1.0 - alpha) * p[n]
+
+		# check convergence, l1 norm
+		err = sum([abs(x[n] - xlast[n]) for n in x])
+		if err < N*tol:
+			return x
+	raise nx.NetworkXError('pagerank: power iteration failed to converge '
+						'in %d iterations.' % max_iter)
--- page_rank.py
+++ page_rank.py
@@ -19,11 +19,11 @@ class Graph:
     def get_matrix(self):
         return self.adjMatrix
 
-n = 5 # number of nodes
+n = 6 # number of nodes
 G = Graph(n)
 
 def out_degree(matrix):
-    outdegree = [0]*5
+    outdegree = [0]*6
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             outdegree[i] = outdegree[i] + matrix[i][j]
@@ -35,19 +35,22 @@ G.set_vertex(1,2)
 G.set_vertex(2,3)
 G.set_vertex(3,4)
 G.set_vertex(4,5)
+G.set_vertex(5,6)
 
 G.set_edge(1,2,1)
-G.set_edge(2,3,1)
-G.set_edge(2,4,1)
+G.set_edge(1,3,1)
+G.set_edge(3,1,1)
+G.set_edge(3,5,1)
 G.set_edge(3,2,1)
-G.set_edge(4,3,1)
 G.set_edge(4,5,1)
-G.set_edge(4,1,1)
-G.set_edge(5,1,1)
+G.set_edge(4,6,1)
+G.set_edge(5,6,1)
+G.set_edge(5,4,1)
+G.set_edge(6,4,1)
 
 # first iteration, give page rank to each node 1/n
 # create a dictionary for each node
-old_pg = [1/n]*5
+old_pg = [1/n]*6
 
 # number of iterations is 7
 # first iteration is done
@@ -55,7 +58,7 @@ old_pg = [1/n]*5
 # jis jis nodes se ith node receive hota hai unpar formula use karna
 adj_mat = G.get_matrix()
 outdegree = out_degree(adj_mat)
-outdegree_norm=[0]*5
+outdegree_norm=[0]*6
 for i in range(len(outdegree)):
     if outdegree[i] == 0:
         outdegree_norm[i] = 0
@@ -72,20 +75,30 @@ def matrix_to_list(matrix):
     return adj
 
 adj = matrix_to_list(G.get_matrix())
-print(adj)
+#print(adj)
 
-for i in range(2):
-    new_old_pg = [0]*5
+for i in range(7):
+    new_old_pg = [0]*6
     for j in range(len(adj)):
         sum_pg = 0
-        print(adj[j])
+        #print(adj[j])
         for k in adj[j]:
-            print(i)
-            print(old_pg[k])
-            print(outdegree_norm[k-1])
+            #print(i)
+            #print(old_pg[k])
+            #print(outdegree_norm[k-1])
             temp = old_pg[k] * outdegree_norm[k]
             sum_pg += temp
         new_old_pg[j] = sum_pg
+    print(f'Iteration {i+1}')
+    print(new_old_pg)
     old_pg = new_old_pg
-    
-print(old_pg)
+
+page_rank = {}
+for i in range(1,7):
+    page_rank[i] = old_pg[i-1]
+sorted_page_rank = {k: v for k, v in sorted(page_rank.items(), key = lambda item: item[1])}
+index = list(sorted_page_rank.items())
+print("Fnial iteration results = ", sorted_page_rank)
+for i in range(len(index)):
+    print(f'Page rank {i+1} is for node {index[i]}')
+print("19BCE0506 Pratyush Kumar")
